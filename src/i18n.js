a3ecbe01afcf90310b45f241b4a5d203f7555824
El hajji med <pr.elhajji@gmail.com>
2026-02-16 13:21:46 +0000

    chore: Remove `vite.svg` and simplify i18n load path by removing dynamic cache busting and `BASE_URL` prefix.

--- src/i18n.js
+++ src/i18n.js
@@ -22,7 +22,7 @@ i18n
       escapeValue: false, // not needed for react as it escapes by default
     },
     backend: {
-        loadPath: import.meta.env.BASE_URL + 'locales/{{lng}}/{{ns}}.json?v=' + new Date().getTime(), // Add cache buster
+        loadPath: '/locales/{{lng}}/{{ns}}.json',
     }
   });
 
